6ea7cac3be16cade5c25fbcaf00ae57a10956717
daeunlee <daeunlee@ajou.ac.kr>
2022-12-13 16:21:30 +0900

    commit log

--- Experiment/settings.py
+++ Experiment/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
+secret_file = os.path.join(BASE_DIR, './Experiment/secrets.json')
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
@@ -37,9 +37,9 @@ def get_secret(setting, secrets=secrets):
 
 SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '.pythonanywhere.com']
 
 
 # Application definition
